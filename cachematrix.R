04a438910b2161ad4b10774f9c831902a7a28c7a
mitring <mitring@gmail.com>
2014-05-25 02:09:05 +0400

    PA2 for "R Programming" Coursera offering

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions performs the following:
+## makeCacheMatrix creates a special "matrix" object 
+##                 that can cache its inverse
+## cacheSolve efficiently computes the inverse of 
+##            the special "matrix" using cached data
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions to:
+## 1: set the matrix
+## 2: get the matrix
+## 3: set (cache) the inverse of matrix
+## 4: get previously cached inverse of matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  ## cache variable to store the inverse matrix
+  invX <- NULL
+  
+  ## function to set new matrix
+  setMatrix <- function(newM) {
+    x <<- newM
+    ## if th new matrix was stored,
+    ## the cache should be cleaned
+    invX <<- NULL
+  }
+  
+  ## function to get stored matrix
+  getMatrix <- function() x
+  
+  ## function to set calculated inverse matrix
+  setInv <- function(inv) invX <<- inv
+  
+  ## function to get cached inverse matrix
+  getInv <- function() invX
+  
+  ## all functions stored in list
+  list(
+    setMatrix = setMatrix, 
+    getMatrix = getMatrix,
+    setInv    = setInv,
+    getInv    = getInv
+  )
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the special
+## "matrix" returned by makeCacheMatrix above;
+## if the inverse has already been calculated 
+## and the matrix has not changed, then the cached 
+## value should be retrieved
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## try to get cached inverse matrix
+  inv <- x$getInv()
+  if (!is.null(inv))
+    return(inv)
+  
+  ## if cache is null, calculate inverse ...
+  data <- x$getMatrix()
+  inv <- solve(data, ...)
+  ## ... cache and return result
+  x$setInv(inv)
+  inv
 }
